a5d3dd5ce372ff2cf03ddf1acc2331cc824855f4
klml <m@klml.de>
2013-09-12 11:52:56 +0200

    rm credits

--- _template/footer.php
+++ _template/footer.php
@@ -1,4 +1,4 @@
-	<footer id="footer">Powered by <a href="http://github.com/lonescript/php-site-maker" target="_blank">php-site-maker</a></footer>
+	<footer id="footer"></footer>
 </div>
 </body>
 </html>
