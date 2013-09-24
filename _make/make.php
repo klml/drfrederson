65cc2f110042910051298566f7c9b466358ecfda
klml <m@klml.de>
2013-09-24 21:49:55 +0200

    typo

--- _make/make.php
+++ _make/make.php
@@ -98,7 +98,7 @@ class MakeSite {
             $page['filenamepath'] = $filenamepath ;
             $page['comment'] = $tmpInfo['comment'];
 
-            $page['pagedurable'] = Markdown( file_get_contents($this->config['pagedurable']) );z
+            $page['pagedurable'] = Markdown( file_get_contents($this->config['pagedurable']) );
             $page['content'] = Markdown( $ymlMD[0] ) ;
 
             // <!-- more --> cutter //~ TODO move to outermarkdown
