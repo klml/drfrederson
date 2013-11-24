75fd9d9e5dc5ac07940a7daa9efb1ea278014035
klml <m@klml.de>
2013-11-24 14:14:35 +0100

    cleanup templates

--- _make/write.jquery.js
+++ _make/write.jquery.js
@@ -24,7 +24,7 @@ var formwebedit = 'form.webedit'; // to formwebedit.php
 
 jQuery(document).ready(function() {
 
-    $( "#footer" ).load( "_template/formwebedit.php", function() {
+    $( "#footer" ).load( "_template/formwebedit.html", function() {
 
         $('#edit').click( function() {
             $(this).hide();
