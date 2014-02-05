85ace451b72aadf7bf61c722872228ca5e46aa27
klml <m@klml.de>
2014-02-05 21:10:34 +0100

    if 2 meta seperator

--- _drf/lib/Tools.php
+++ _drf/lib/Tools.php
@@ -33,6 +33,7 @@ function preventDirectoryTraversal($sourcepath) {
 function splitYamlProse($filePath, $separator) {
     $ymlProse = explode( $separator , file_get_contents($filePath) ); // TODO limit ?
     if ( count($ymlProse) == 1 ) $ymlProse[1] = '';
+    $ymlProse = array_slice( $ymlProse, 0, 2 );
     $ymlProse = array_combine( array('prose', 'meta' ) , $ymlProse );
     return $ymlProse ; 
 }
