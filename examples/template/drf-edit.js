68ad68a54f5531aa634d2f005d5754f25f6c24c4
klml <m@klml.de>
2015-03-23 20:58:21 +0100

    jquery.cookie

--- examples/template/drf-edit.js
+++ examples/template/drf-edit.js
@@ -2,25 +2,23 @@ jQuery(document).ready(function() {
 
     $('#requestauth, #showedit').click( function() {
         if( $(this).is(':checked') ) {
-            document.cookie = 'showedit=true' ;
+            $.cookie('showedit', true);
         };
         if( !$(this).is(':checked') ) {
-            document.cookie = 'showedit=false';
+            $.cookie('showedit', false);
             $('#drf-edit a').hide();
         };
     });
 
-    edit = document.cookie.search(/showedit=true/); 
-    if( edit != -1 ) {
+    if( $.cookie('showedit') == "true" ) {
         $('#drf-edit a').show();
         $('#requestauth, #showedit').prop('checked', true);
     };
-    // TODO useage -- plugin http://cdn.jsdelivr.net/jquery.cookie/1.4.1/jquery.cookie.js
-
 
    (this.hashrouter = function () {
         var hash = window.location.hash ;
         if ( hash == "#drf-edit" || hash == "#drf-markdownwysiwym" ) {
+            $.cookie('showedit', true);
             $( "#drf-edit" ).load( "_drf/drf-formwebedit.html")
         }
    })();
