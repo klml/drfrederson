a42eedd816777bd5b7234c2a9c9a466d8d90e9d9
Klaus Mueller <m@klml.de>
2018-04-07 19:28:06 +0200

    rm not needed

--- _drf/lib/Tools.php
+++ _drf/lib/Tools.php
@@ -63,28 +63,4 @@ function wikistylelinks($content) {
     return $content ;
 }
 
-// <!-- more --> cutter
-function moreCutter($content) {
-    $more = explode('<!--more-->', $content);
-    if (count($more) >= 2) {
-        $more = $more[0];
-    } else {
-        $more = false;
-    }
-    return $more ;
-}
-
-// create *.html  // TODO remove
-function makeHtmlFile($target, $layout, $data) {
-	ob_start();
-	require $layout;
-	$html = ob_get_contents();
-	ob_end_clean();
-	$fp = fopen($target, 'w');
-	if ($fp) {
-		fwrite($fp, $html);
-		fclose($fp);
-	}
-}
-
 ?>
